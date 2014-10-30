6ba07492d85aa4e4f2747d3b407b8e341814552b
JCPedroza <nemesis4go10@hotmail.com>
2014-10-30 12:16:11 -0600

    improved indentation

--- SingleFragmentActivity.java
+++ SingleFragmentActivity.java
@@ -1,5 +1,3 @@
-package com.bignerdranch.android.criminalintent;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -13,23 +11,22 @@ public abstract class SingleFragmentActivity extends FragmentActivity {
      protected Fragment createFragment() {
          return OneFragment.newInstance(<data>); }
     */
-	protected abstract Fragment createFragment();
+    protected abstract Fragment createFragment();
 	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_fragment);
-		FragmentManager fm = getSupportFragmentManager();
-		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-		
-		if (fragment == null) {
-			fragment = createFragment();
-			fm.beginTransaction()
-			    .add(R.id.fragmentContainer, fragment)
-			    .commit();
-		}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_fragment);
+	FragmentManager fm = getSupportFragmentManager();
+	Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+	
+	if (fragment == null) {
+	    fragment = createFragment();
+	    fm.beginTransaction()
+	    .add(R.id.fragmentContainer, fragment)
+	    .commit();
 	}
-	
+    }
 }
 
 // Notes: 
